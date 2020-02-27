d995d61f7908d0e306b787cb33c75b3d91835aa5
Jeremy Chang <jeremy60317@Jeremyde-MacBook-Pro.local>
2020-02-27 23:33:16 +0800

    delete console 20200227

--- src/App.js
+++ src/App.js
@@ -150,7 +150,7 @@ function App() {
         setMapData(mapArr);
         // setTest(test);
         setIsLoading(false);
-        // console.log("create by Jeremy Chang");
+        console.log("create by Jeremy Chang");
       })
       .catch(err => setErrText("好像壞了"));
   }, []);
